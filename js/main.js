77b1d9010d438ae2296afcf6f04eff8659d8c20c
gbr.ahmed <gabr18341@gmail.com>
2024-12-12 20:49:59 +0300

    last update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
 const currentPage = window.location.pathname
 let currentLanguage = localStorage.getItem('lang') || 'en';
 
-// handel language change
 
-function changeLang(lang) {
-    localStorage.setItem('lang', lang);
-    document.documentElement.lang = lang;
-    currentLanguage = lang;
-
-    if (lang === 'en') {
-        document.body.classList.remove('rtl')
-        document.body.dir = 'ltr';
-        
-    } else {
-        document.body.classList.add('rtl')
-        document.body.dir = 'rtl'
-    }
-}
-
-changeLang(currentLanguage);
 
 document.querySelector('header').innerHTML = `
 <div class="container p-2">
@@ -72,8 +55,8 @@ document.querySelector('header').innerHTML = `
             English (en)
         </button>
         <ul class="dropdown-menu">
-        <li><span onclick="changeLang('en')" class="dropdown-item active" >en</span></li>
-            <li><span onclick="changeLang('ar')" class="dropdown-item " >ar</span></li>
+        <li><span onclick="changeLang('en')" class="dropdown-item active en" >en</span></li>
+            <li><span onclick="changeLang('ar')" class="dropdown-item  ar" >ar</span></li>
         </ul>
 
         <a href="tel:+966920012778" class="main-btn">
@@ -132,6 +115,28 @@ document.querySelector('footer').innerHTML = `
             </div>
         </div>
 `
+// handel language change
+
+function changeLang(lang) {
+    localStorage.setItem('lang', lang);
+    document.documentElement.lang = lang;
+    currentLanguage = lang;
+
+    if (lang === 'en') {
+        document.body.classList.remove('rtl')
+        document.body.dir = 'ltr';
+        document.querySelector('header .dropdown-menu .dropdown-item.en').classList.add('active')
+        document.querySelector('header .dropdown-menu .dropdown-item.ar').classList.remove('active')
+    } else {
+        document.body.classList.add('rtl')
+        document.body.dir = 'rtl'
+        document.querySelector('header .dropdown-menu .dropdown-item.ar').classList.add('active')
+        document.querySelector('header .dropdown-menu .dropdown-item.en').classList.remove('active')
+    }
+}
+
+changeLang(currentLanguage);
+
 // handel show mobile menu
 document.getElementById('mobile-menu').onclick = () => {
     document.querySelector('.nav-header').classList.add('show-mobile-menu');
@@ -146,4 +151,149 @@ function showCart() {
 }
 
 
+// handel count in cart 
+function handelCountInCart(btn) {
+    let count = btn.parentNode.querySelector('.count').textContent;
+    let calc = btn.getAttribute('data-calc')
+    if (calc === 'dec') {
+        if (+count > 1) {
+            count--;
+        }
+    } else {
+        +count++;
+    }
+    btn.parentNode.querySelector('.count').textContent = count;
+}
 
+
+// handel filter section
+
+const shopData = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'development',
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'development',
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'development',
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'development',
+    },
+    {
+        id: 5,
+        name: 'Product 5',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'advertising',
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'advertising',
+    },
+    {
+        id: 7,
+        name: 'Product 7',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'services',
+    },
+    {
+        id: 8,
+        name: 'Product 8',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'services',
+    },
+    {
+        id: 9,
+        name: 'Product 9',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'services',
+    },
+    {
+        id: 10,
+        name: 'Product 10',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'services',
+    },
+    {
+        id: 11,
+        name: 'Product 11',
+        price: 200,
+        img: "https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1",
+        category: 'services',
+    },
+]
+
+const filterBtns = document.querySelectorAll('.shop-section ul button');
+
+function filterData(data) {
+    const shopContainer = document.querySelector('.shop-section .shop-container')
+    shopContainer.innerHTML = '';
+    if (!data.length) {
+        shopContainer.innerHTML = '<h3 class="text-center">No products found</h3>'
+        return;
+
+    }
+    data.map(item => {
+        return(
+            shopContainer.innerHTML += `
+                <div class="product-item p-2 d-flex align-items-center gap-2">
+                    <div class="img-container">
+                        <img src="https://i0.wp.com/dra.sa/wp-content/uploads/2024/07/Package-1.png?fit=1080%2C1080&ssl=1"
+                            alt="">
+                        <div class="add-cart"><span onclick="showCart()">+</span></div>
+                    </div>
+                    <div class="product-content p-1">
+                        <a href="product.html"><h6>Growth Package</h6></a>
+                        <div class="stars-box d-flex align-items-center g-1 mb-2">
+                            <i class='bx bx-star'></i>
+                            <i class='bx bx-star'></i>
+                            <i class='bx bx-star'></i>
+                            <i class='bx bx-star'></i>
+                            <i class='bx bx-star'></i>
+                        </div>
+                        <span class="price">575.00 رس</span>
+                    </div>
+                </div>
+            `
+        )
+    })
+}
+
+filterBtns?.forEach((btn) => {
+    btn.addEventListener('click' , (btn) => {
+        const targetCategory = btn.target.getAttribute('data-filter');
+        const dataFiltered = shopData.filter(item => {
+            if (item.category === targetCategory) {
+                return item;
+            } else if ('all' === targetCategory) {
+                return shopData;
+            }
+        });
+        filterData(dataFiltered)
+    })
+})
--- js/product.js
+++ js/product.js
@@ -7,32 +7,32 @@ document.querySelector('.popup-img .close').addEventListener('click', function (
 
 // handel zoom in img on hover
 
-const container = document.querySelector(".product-container .product-img");
-const image = document.querySelector(".zoom-image");
-const lens = document.querySelector(".zoom-lens");
+// const container = document.querySelector(".product-container .product-img");
+// const image = document.querySelector(".zoom-image");
+// const lens = document.querySelector(".zoom-lens");
 
-container.addEventListener("mousemove", (e) => {
-    const bounds = container.getBoundingClientRect();
-    const lensSize = lens.offsetWidth / 2;
+// container.addEventListener("mousemove", (e) => {
+//     const bounds = container.getBoundingClientRect();
+//     const lensSize = lens.offsetWidth / 2;
 
-    // حساب موضع العدسة
-    let x = e.clientX - bounds.left - lensSize;
-    let y = e.clientY - bounds.top - lensSize;
+//     // حساب موضع العدسة
+//     let x = e.clientX - bounds.left - lensSize;
+//     let y = e.clientY - bounds.top - lensSize;
 
-    // الحد من حركة العدسة داخل الصورة
-    x = Math.max(0, Math.min(x, bounds.width - lens.offsetWidth));
-    y = Math.max(0, Math.min(y, bounds.height - lens.offsetHeight));
+//     // الحد من حركة العدسة داخل الصورة
+//     x = Math.max(0, Math.min(x, bounds.width - lens.offsetWidth));
+//     y = Math.max(0, Math.min(y, bounds.height - lens.offsetHeight));
 
-    lens.style.left = `${x}px`;
-    lens.style.top = `${y}px`;
-    lens.style.display = "block";
+//     lens.style.left = `${x}px`;
+//     lens.style.top = `${y}px`;
+//     lens.style.display = "block";
 
-    // تطبيق تأثير التكبير
-    image.style.transformOrigin = `${(x / bounds.width) * 100}% ${(y / bounds.height) * 100}%`;
-    image.style.transform = "scale(2)"; // تكبير الصورة
-});
+//     // تطبيق تأثير التكبير
+//     image.style.transformOrigin = `${(x / bounds.width) * 100}% ${(y / bounds.height) * 100}%`;
+//     image.style.transform = "scale(2.5)"; // تكبير الصورة
+// });
 
-container.addEventListener("mouseleave", () => {
-    lens.style.display = "none";
-    image.style.transform = "scale(1)"; // إعادة الصورة إلى الحجم الطبيعي
-});
+// container.addEventListener("mouseleave", () => {
+//     lens.style.display = "none";
+//     image.style.transform = "scale(1)"; // إعادة الصورة إلى الحجم الطبيعي
+// });
